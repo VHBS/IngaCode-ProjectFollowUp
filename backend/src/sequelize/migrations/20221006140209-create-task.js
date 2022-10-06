fdf5643cbe69aa6d34b59b2ece64daeabba2d531
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-06 16:43:54 -0300

    feat: created task migration

---
+++ backend/src/sequelize/migrations/20221006140209-create-task.js
@@ -0,0 +1,46 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      projectId: {
+        type: Sequelize.UUID,
+        references: {
+          model: 'Projects',
+          key: 'id',
+        },
+        field: 'project_id',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        field: 'updated_at',
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        field: 'deleted_at',
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Tasks');
+  },
+};
