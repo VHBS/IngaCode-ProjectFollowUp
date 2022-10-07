4f0ffd764830095813c21851f1bec9885be4c8da
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-07 11:00:52 -0300

    feat: created collaborator migration

---
+++ backend/src/sequelize/migrations/20221006202157-create-collaborator.js
@@ -0,0 +1,45 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Collaborators', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING(250),
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.UUID,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        field: 'user_id',
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        field: 'created_at',
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+        field: 'updated_at',
+      },
+      deletedAt: {
+        type: Sequelize.DATE,
+        field: 'deleted_at',
+      },
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Collaborators');
+  },
+};
