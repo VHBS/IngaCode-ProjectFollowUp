9e75515dcb776790bc98e5555f455060f0fc1770
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-07 10:56:47 -0300

    fix: updated user seeder

--- backend/src/sequelize/seeders/20221005234716-demo-user.js
+++ backend/src/sequelize/seeders/20221005234716-demo-user.js
@@ -1,13 +1,19 @@
-const { v4: uuidv4 } = require('uuid');
 /** @type {import('sequelize-cli').Migration} */
 
 module.exports = {
   async up(queryInterface) {
     return queryInterface.bulkInsert('Users', [
       {
-        id: uuidv4(),
+        id: 'd8723224-e445-4ccc-aeb3-a4ad55462c84',
         user_name: 'John',
         password: '123456789',
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        id: '23a5999d-dbfc-4960-ac4d-9ec2b91a302c',
+        user_name: 'Victor',
+        password: '987654321',
         created_at: new Date(),
         updated_at: new Date(),
       },
