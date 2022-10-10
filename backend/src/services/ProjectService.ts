ca28a7fe4e5090176eae4fa11e01617ab8caac6d
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-10 16:50:15 -0300

    feat: update project service created

--- backend/src/services/ProjectService.ts
+++ backend/src/services/ProjectService.ts
@@ -1,16 +1,31 @@
-import { InputProjectType, ProjectType } from '../@types/project';
-import IProject from '../sequelize/models/interfaces/IProject';
+import { InputProjectType } from '../@types/project';
+import { ProjectServiceType } from '../@types/projectService';
 import Project from '../sequelize/models/Project';
-import { IService } from './interfaces/IService';
+import { IProjectService } from './interfaces/IProjectService';
 
-export default class ProjectService implements IService<ProjectType> {
-  public findAll = async (): Promise<IProject[]> => {
+export default class ProjectService implements IProjectService<ProjectServiceType> {
+  public findAll = async (): Promise<ProjectServiceType> => {
     const allProjects = await Project.findAll();
-    return allProjects;
+    return { status: 200, json: allProjects };
   };
 
-  public create = async (entity: InputProjectType) => {
+  public create = async (entity: InputProjectType): Promise<ProjectServiceType> => {
     const newProject = await Project.create(entity);
-    return newProject;
+    return { status: 200, json: newProject };
+  };
+
+  public update = async (id: string, entity: InputProjectType): Promise<ProjectServiceType> => {
+    const { name } = entity;
+    const findProjectById = await Project.findByPk(id);
+
+    if (!findProjectById) {
+      return { status: 404, json: { message: 'Project not found!' } };
+    }
+
+    await findProjectById.update({
+      name,
+      updatedAt: new Date(),
+    });
+    return { status: 200, json: findProjectById };
   };
 }
