16fdf0eba8eb501c839fa0267da3472de4b07992
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-10 12:51:53 -0300

    feat: create project controller created

--- backend/src/controllers/ProjectController.ts
+++ backend/src/controllers/ProjectController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { ProjectType } from '../@types/project';
+import { ResponseError } from '../@types/responseError';
 import { IService } from '../services/interfaces/IService';
 import { IController } from './interfaces/IController';
 
@@ -18,6 +19,21 @@ export default class ProjectController implements IController<ProjectType> {
     try {
       const allProjects = await this._service.findAll();
       return res.status(200).json(allProjects);
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  public create = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response<ProjectType | ResponseError> | void> => {
+    try {
+      const { name } = req.body;
+      const newProject = await this._service.create({ name });
+
+      return res.status(200).json(newProject);
     } catch (error) {
       return next(error);
     }
--- backend/src/controllers/interfaces/IController.ts
+++ backend/src/controllers/interfaces/IController.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
+import { ResponseError } from '../../@types/responseError';
 
 export interface IController<T> {
   findAll: (req: Request, res: Response, next: NextFunction) => Promise<Response<T[]> | void>;
-  // create: (req: Request, res: Response, next: NextFunction) => Promise<Response<T>>;
+  create: (req: Request, res: Response, next: NextFunction) => Promise<Response<T | ResponseError> | void>;
   // update: (req: Request, res: Response, next: NextFunction) => Promise<Response<T>>;
   // findOne: (req: Request, res: Response, next: NextFunction) => Promise<Response<T>>;
   // delete: (req: Request, res: Response, next: NextFunction) => Promise<Response<void>>;
