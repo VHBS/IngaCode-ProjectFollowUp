b2eed8e261495d79b7c458a2c296877b1a16e268
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-10 10:13:13 -0300

    fix: updated user route

--- backend/src/routes/userRouter.ts
+++ backend/src/routes/userRouter.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import UserController from '../controllers/UserController';
+import UserMiddleware from '../middlewares/IUserMiddleware';
 import UserService from '../services/UserService';
 
 const userRoutes = Router();
 
+const userMiddleware = new UserMiddleware();
 const userService = new UserService();
 const userController = new UserController(userService);
 
-userRoutes.post('/login', userController.login);
+userRoutes.post('/login', userMiddleware.login, userController.login);
 
 export default userRoutes;
