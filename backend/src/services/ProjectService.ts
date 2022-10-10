af285e865e91482afb37a37dfcf0f33adbc90ec1
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-10 18:01:44 -0300

    feat: delete project service created

--- backend/src/services/ProjectService.ts
+++ backend/src/services/ProjectService.ts
@@ -4,6 +4,8 @@ import Project from '../sequelize/models/Project';
 import { IProjectService } from './interfaces/IProjectService';
 
 export default class ProjectService implements IProjectService<ProjectServiceType> {
+  public projectNotFound = 'Project not found!';
+
   public findAll = async (): Promise<ProjectServiceType> => {
     const allProjects = await Project.findAll();
     return { status: 200, json: allProjects };
@@ -19,13 +21,31 @@ export default class ProjectService implements IProjectService<ProjectServiceTyp
     const findProjectById = await Project.findByPk(id);
 
     if (!findProjectById) {
-      return { status: 404, json: { message: 'Project not found!' } };
+      return { status: 404, json: { message: this.projectNotFound } };
     }
 
     await findProjectById.update({
       name,
       updatedAt: new Date(),
     });
+    return { status: 200, json: findProjectById };
+  };
+
+  public delete = async (id: string): Promise<ProjectServiceType> => {
+    const findProjectById = await Project.findByPk(id);
+
+    if (!findProjectById) {
+      return { status: 404, json: { message: this.projectNotFound } };
+    }
+
+    if (findProjectById.deletedAt) {
+      return { status: 400, json: { message: 'Project has been deleted' } };
+    }
+
+    await findProjectById.update({
+      deletedAt: new Date(),
+    });
+
     return { status: 200, json: findProjectById };
   };
 }
--- backend/src/services/interfaces/IService.ts
+++ backend/src/services/interfaces/IService.ts
@@ -1,3 +1,4 @@
 export interface IService<T> {
   findAll: () => Promise<T>;
+  delete: (id: string) => Promise<T>;
 }
