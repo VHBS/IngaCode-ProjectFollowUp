4e23adb3390d36f742d19f9a90690d628b10694c
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-11 15:15:59 -0300

    feat: delete timeTracker service created

--- backend/src/services/TimeTrackerService.ts
+++ backend/src/services/TimeTrackerService.ts
@@ -42,4 +42,22 @@ export default class TimeTrackerService implements ITimeTrackerService<TimeTrack
     });
     return { status: 200, json: findTimeTrackerById };
   };
+
+  public delete = async (id: string): Promise<TimeTrackerServiceType> => {
+    const findTimeTrackerById = await TimeTracker.findByPk(id);
+
+    if (!findTimeTrackerById) {
+      return { status: 404, json: { message: this.timeTrackerNotFound } };
+    }
+
+    if (findTimeTrackerById.deletedAt) {
+      return { status: 400, json: { message: this.timeTrackerAlreadyDeleted } };
+    }
+
+    await findTimeTrackerById.update({
+      deletedAt: new Date(),
+    });
+
+    return { status: 204, json: findTimeTrackerById };
+  };
 }
