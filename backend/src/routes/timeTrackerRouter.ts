85a2c6d8d1b3eb35d114660affc7bf1be85d475a
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-11 15:16:38 -0300

    feat: delete timeTracker route created

--- backend/src/routes/timeTrackerRouter.ts
+++ backend/src/routes/timeTrackerRouter.ts
@@ -12,5 +12,6 @@ const timeTrackerController = new TimeTrackerController(timeTrackerService);
 timeTrackerRoutes.get('/', timeTrackerController.findAll);
 timeTrackerRoutes.post('/', timeTrackerMiddleware.create, timeTrackerController.create);
 timeTrackerRoutes.patch('/:id', timeTrackerMiddleware.update, timeTrackerController.update);
+timeTrackerRoutes.delete('/:id', timeTrackerMiddleware.delete, timeTrackerController.delete);
 
 export default timeTrackerRoutes;
