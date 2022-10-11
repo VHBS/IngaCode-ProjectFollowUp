43f4816298e298577a35dc272b7c4578d06c16a6
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-11 11:20:36 -0300

    feat: update task service created

--- backend/src/services/TaskService.ts
+++ backend/src/services/TaskService.ts
@@ -1,4 +1,4 @@
-import { InputTaskType } from '../@types/task';
+import { InputTaskType, TaskType } from '../@types/task';
 import { TaskServiceType } from '../@types/taskService';
 import Task from '../sequelize/models/Task';
 import { ITaskService } from './interfaces/ITaskService';
@@ -21,6 +21,28 @@ export default class TaskService implements ITaskService<TaskServiceType> {
   public create = async (entity: InputTaskType): Promise<TaskServiceType> => {
     const newTask = await Task.create(entity);
     return { status: 201, json: newTask };
+  };
+
+  public update = async (id: string, entity: TaskType): Promise<TaskServiceType> => {
+    console.log('service');
+    const { name, description, projectId } = entity;
+    const findTaskById = await Task.findByPk(id);
+
+    if (!findTaskById) {
+      return { status: 404, json: { message: this.taskNotFound } };
+    }
+
+    if (findTaskById.deletedAt) {
+      return { status: 400, json: { message: this.taskAlreadyDeleted } };
+    }
+
+    await findTaskById.update({
+      name,
+      description,
+      projectId,
+      updatedAt: new Date(),
+    });
+    return { status: 200, json: findTaskById };
   };
 
   public delete = async (id: string): Promise<TaskServiceType> => {
--- backend/src/services/interfaces/ITaskService.ts
+++ backend/src/services/interfaces/ITaskService.ts
@@ -1,6 +1,7 @@
-import { InputTaskType } from '../../@types/task';
+import { InputTaskType, TaskType } from '../../@types/task';
 import { IService } from './IService';
 
 export interface ITaskService<T> extends IService<T> {
   create: (entity: InputTaskType) => Promise<T>;
+  update: (id: string, entity: TaskType) => Promise<T>;
 }
