6ba0499e42e3ca3a30070510a11b1d6ff896bb15
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-11 10:41:11 -0300

    feat: findAll and create task controller created

---
+++ backend/src/controllers/TaskController.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from 'express';
+import { ResponseError } from '../@types/responseError';
+import { TaskType } from '../@types/task';
+import { TaskServiceType } from '../@types/taskService';
+import { ITaskService } from '../services/interfaces/ITaskService';
+// import { IController } from './interfaces/IController';
+
+// export default class TaskController implements IController<TaskType> {
+export default class TaskController {
+  private _service: ITaskService<TaskServiceType>;
+
+  constructor(service: ITaskService<TaskServiceType>) {
+    this._service = service;
+  }
+
+  public findAll = async (_req: Request, res: Response, next: NextFunction): Promise<Response<TaskType[]> | void> => {
+    try {
+      const resultTaskService = await this._service.findAll();
+      return res.status(resultTaskService.status).json(resultTaskService.json);
+    } catch (error) {
+      return next(error);
+    }
+  };
+
+  public create = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<Response<TaskType> | ResponseError | void> => {
+    try {
+      const { name, description, projectId } = req.body;
+      const resultTaskService = await this._service.create({ name, description, projectId });
+      return res.status(resultTaskService.status).json(resultTaskService.json);
+    } catch (error) {
+      return next(error);
+    }
+  };
+}
