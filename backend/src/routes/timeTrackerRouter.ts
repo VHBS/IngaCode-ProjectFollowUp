089a9cf25bfff3d4d8f819eab5b38cc02d944329
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-11 14:29:02 -0300

    feat: create timeTracker route created

--- backend/src/routes/timeTrackerRouter.ts
+++ backend/src/routes/timeTrackerRouter.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import TimeTrackerController from '../controllers/TimeTrackerController';
+import TimeTrackerMiddleware from '../middlewares/TimeTrackerMiddleware';
 import TimeTrackerService from '../services/TimeTrackerService';
 
 const timeTrackerRoutes = Router();
 
+const timeTrackerMiddleware = new TimeTrackerMiddleware();
 const timeTrackerService = new TimeTrackerService();
 const timeTrackerController = new TimeTrackerController(timeTrackerService);
 
 timeTrackerRoutes.get('/', timeTrackerController.findAll);
+timeTrackerRoutes.post('/', timeTrackerMiddleware.create, timeTrackerController.create);
 
 export default timeTrackerRoutes;
