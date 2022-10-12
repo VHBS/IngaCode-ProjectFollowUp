9ac25b03ec905440b37dd2538b4f226329a6fbdc
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-12 14:33:54 -0300

    feat: delete project interface

--- frontend/src/pages/ProjectDetails.tsx
+++ frontend/src/pages/ProjectDetails.tsx
@@ -1,18 +1,32 @@
 import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
+import { AuthContextType } from '../@types/authContext';
 import Navbar from '../components/Navbar';
 import UpdateProject from '../components/UpdateProject';
+import useAuth from '../hooks/useAuth';
+import { handleFetchDeleteProject } from '../utils/api';
 
 export default function ProjectDetails(): JSX.Element {
   const [showModalUpdateProject, setShowModalUpdateProject] = useState<boolean>(false);
-
+  const { userData } = useAuth() as AuthContextType;
   const { id } = useParams();
+  const navigate = useNavigate();
+
+  const handleDeleteProject = async () => {
+    if (userData) {
+      await handleFetchDeleteProject(userData.token, id as string);
+      navigate('/projects');
+    }
+  };
   return (
     <div>
       <Navbar />
       <button type="button" onClick={() => setShowModalUpdateProject(!showModalUpdateProject)}>
         Edit Project
+      </button>
+      <button type="button" onClick={() => handleDeleteProject()}>
+        Delete Project
       </button>
       {showModalUpdateProject && <UpdateProject props={{ setShowModalUpdateProject }} />}
     </div>
