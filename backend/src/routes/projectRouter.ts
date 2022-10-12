7223034144563de2b9257826d9a683c314a5ffb9
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-12 16:51:34 -0300

    feat: find one project route created

--- backend/src/routes/projectRouter.ts
+++ backend/src/routes/projectRouter.ts
@@ -16,5 +16,6 @@ projectRoutes.get('/', userMiddleware.validateJwt, projectController.findAll);
 projectRoutes.post('/', userMiddleware.validateJwt, projectMiddleware.create, projectController.create);
 projectRoutes.patch('/:id', userMiddleware.validateJwt, projectMiddleware.update, projectController.update);
 projectRoutes.delete('/:id', userMiddleware.validateJwt, projectMiddleware.delete, projectController.delete);
+projectRoutes.get('/:id', userMiddleware.validateJwt, projectController.findOne);
 
 export default projectRoutes;
