2154ab91b661d668363c95f7557a95664e54f46b
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-12 20:21:27 -0300

    feat: private router created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,14 +5,17 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import ProjectDetails from './pages/ProjectDetails';
 import Projects from './pages/Projects';
+import PrivateRouter from './router/PrivateRouter';
 
 export default function App(): JSX.Element {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/projects/:id" element={<ProjectDetails />} />
+      <Route path="/" element={<PrivateRouter />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/projects/:id" element={<ProjectDetails />} />
+      </Route>
     </Routes>
   );
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,20 +1,8 @@
-import React, { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
-import { AuthContextType } from '../@types/authContext';
 import Navbar from '../components/Navbar';
-import useAuth from '../hooks/useAuth';
 
 export default function Home(): JSX.Element {
-  const navigate = useNavigate();
-  const { userData } = useAuth() as AuthContextType;
-
-  useEffect(() => {
-    if (userData === null) {
-      return navigate('/login');
-    }
-  }, [userData]);
-
   return (
     <div>
       <Navbar />
