59d5e3171c1514115f1341718576c231bb3a506f
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-14 22:56:46 -0300

    style: nav bar style created

---
+++ frontend/src/styles/NavBar.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const NavBarComponent = styled.header`
+  background-color: rgba(37, 36, 34, 0.74);
+  top: 0;
+  position: sticky;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: rgb(255, 252, 242);
+  backdrop-filter: blur(0.3em);
+  z-index: 2;
+  h5{
+    margin-left: 1em;
+  }
+  `;
+
+export const ButtonsContainer = styled.div`
+  display: flex;
+  button {
+    margin: 0.5em;
+  }
+  
+`;
