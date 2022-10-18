e19f4720dab76804a5730d204a04e3475d311235
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-17 22:30:13 -0300

    fix: task page updated

--- frontend/src/styles/Task.tsx
+++ frontend/src/styles/Task.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
-import { ProjectsPage } from './PageCards';
+import { PageComponent } from './PageCards';
 
-export const TasksPage = styled(ProjectsPage)`
+export const TasksPage = styled(PageComponent)`
   background: linear-gradient(rgba(134, 134, 134, 0.7), rgba(37, 37, 37, 0.596)), url('https://images.unsplash.com/photo-1665501340269-5ced49f91a92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1974&q=80');
   background-position: center left;
   background-repeat: no-repeat;
