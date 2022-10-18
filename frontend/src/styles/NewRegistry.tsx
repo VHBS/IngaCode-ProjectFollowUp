53d58048f67919aee615b20d6e46d76bd93a2435
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2022-10-17 22:23:10 -0300

    feat: animate show component

--- frontend/src/styles/NewRegistry.tsx
+++ frontend/src/styles/NewRegistry.tsx
@@ -1,15 +1,40 @@
 import styled from 'styled-components';
 
-export const NewItemComponent = styled.div`
-  display: flex;
+export const NewItemComponent = styled.div<{ showModalNewItem: boolean}>`
   margin-bottom: 0.5rem;
+  overflow:hidden;
+  color: rgb(204, 197, 185);
+  
+  
+  ${({ showModalNewItem }) => {
+    if (!showModalNewItem) {
+      return `
+      max-height: 0;
+      transition: max-height 0.2s ease-in-out;
+      pointer-events: none;
+      animation: fadeOut 0.7s;
+      @keyframes fadeOut {
+          from { opacity: 1; }
+          to   { opacity: 0; }
+      };
+    `;
+    }
+    return `
+      max-height: 900px;
+      transition: max-height 0.3s ease-in-out;
+      animation: fadein 0.5s;
+      @keyframes fadein {
+          from { opacity: 0; }
+          to   { opacity: 1; }
+      };
+    `;
+  }}
 `;
 
 export const NewProjectTitle = styled.h2`
   font-size: xx-large;
   text-align: center;
   padding: 1rem;
-  color: rgb(204, 197, 185);
 `;
 
 export const NewItemContainer = styled.div`
@@ -34,4 +59,20 @@ export const NewItemContainer = styled.div`
   button { 
     margin-top: 2rem
   }
+
+  .error-message {
+    span{
+      color: rgb(235, 94, 40);
+      font-weight: 800;
+    }
+  }
+
+  label {
+    border-bottom: 1px solid rgb(255, 252, 242,0.2);
+    padding-block: 1rem;
+  };
+
+  label:nth-last-child(-n+2) {
+    border-bottom: 1px solid rgb(255, 252, 242,0);
+  };
 `;
--- frontend/src/styles/PageCards.tsx
+++ frontend/src/styles/PageCards.tsx
@@ -1,11 +1,40 @@
 import styled from 'styled-components';
 
-export const ProjectsPage = styled.div`
-  background: linear-gradient(rgba(134, 134, 134, 0.7), rgba(37, 37, 37, 0.596)), url('https://images.unsplash.com/photo-1526997440933-f7ec78a27601?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1936&q=80');
+export const PageComponent = styled.div`
+  background: linear-gradient(rgba(134, 134, 134, 0.7), rgba(37, 37, 37, 0.596)), url('https://images.unsplash.com/photo-1476977251893-330046eb5ec8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80');
   background-position: center left;
   background-repeat: no-repeat;
   background-size: cover;
   min-height: 100vh;
+`;
+
+export const FilterComponent = styled.div<{showFilters: boolean}>`
+  overflow:hidden;
+
+  ${({ showFilters }) => {
+    if (!showFilters) {
+      return `
+      max-height: 0;
+      transition: max-height 0.3s ease-in-out;
+      pointer-events: none;
+      animation: fadeOut 1s;
+      @keyframes fadeOut {
+          from { opacity: 1; }
+          to   { opacity: 0; }
+      };
+    `;
+    }
+    return `
+      max-height: 900px;
+      transition: max-height 0.3s ease-in-out;
+      animation: fadein 0.7s;
+      @keyframes fadein {
+          from { opacity: 0; }
+          to   { opacity: 1; }
+      };
+    `;
+  }}
+  
 `;
 
 export const FilterContainer = styled.div`
@@ -24,6 +53,15 @@ export const FilterContainer = styled.div`
   input {
     margin-bottom: 1rem;
   }
+
+  label {
+    border-bottom: 1px solid rgb(255, 252, 242,0.2);
+    padding-block: 1rem;
+  };
+
+  label:last-child {
+    border-bottom: 1px solid rgb(255, 252, 242,0);
+  };
 `;
 
 export const TitlePage = styled.div`
@@ -34,6 +72,7 @@ export const TitlePage = styled.div`
   width: 70%;
   max-width: 20rem;
   margin-inline: auto;
+  
 
   h1 {
     padding: 0.2rem 0.5rem;
@@ -41,6 +80,8 @@ export const TitlePage = styled.div`
     color: rgb(204, 197, 185);
     background-color: rgb(64, 61, 57, 0.7);
     margin-block: 0.5rem;
+    backdrop-filter: blur(16px);
+
   }
 
   .title-container-buttons {
@@ -49,6 +90,8 @@ export const TitlePage = styled.div`
     justify-content: space-evenly;
     width: 100%;
     background-color: rgb(64, 61, 57, 0.7);
+  backdrop-filter: blur(16px);
+
     border-radius: 0.2rem;
     margin-bottom: 0.5rem;
     display: flex;
